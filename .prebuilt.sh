a9549dbe188ea00d48228fca024ca246345cc9ca
tigelake <v.scdmitriy@gmail.com>
2024-12-14 12:50:21 +0300

    Create .prebuilt.sh

---
+++ .prebuilt.sh
@@ -0,0 +1,9 @@
+sudo apt-get update -y
+
+rustup default nightly
+rustup component add rust-src
+
+rustup default stable
+rustup component add rust-src
+
+cargo install cross --git https://github.com/cross-rs/cross
